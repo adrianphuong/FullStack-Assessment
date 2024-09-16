9b6cb64af81994b94b09f0508bd26773d59c5db2
adrian <adrianphuong1@gmail.com>
2024-09-16 16:18:28 -0700

    first commit

---
+++ back-end/Server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 2000;
+
+app.use(express.json());
+app.use(cors());
+
+let globalRole = "";
+let questions = [];
+
+app.get("/api/getrole", async(req,res) => {
+    if(globalRole) {
+        res.send(globalRole);
+    }
+    else {
+        res.status(404).send({message: "No role is defined"})
+    }
+})
+
+app.post("/api/setrole", async(req,res) => {
+    const role = req.body;
+    globalRole = role;
+    try {
+        res.status(200).send(role);
+    }
+    catch(err) {
+        res.status(500).send({message: err});
+    }
+})
+
+app.post("/api/logout",async(req,res) => {
+    globalRole = "";
+})
+
+// Question creation
+
+app.get("/api/getquestions", async(req,res) => {
+    res.send(questions);
+})
+
+app.post("/api/createquestion", async(req,res) => {
+    const {question, difficulty, answerA, answerB, answerC, answerD, correctAnswer} = req.body;
+    try {
+        const newQuestion = {
+            question: question,
+            difficulty: difficulty,
+            answerA: answerA,
+            answerB: answerB,
+            answerC: answerC,
+            answerD: answerD,
+            correctAnswer: correctAnswer
+        }
+        questions.push(newQuestion);
+        res.send(questions)
+    }
+    catch(err) {
+        res.status(500).send({message: err});
+    }
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on " + PORT);
+});
